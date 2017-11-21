02c8cfd75e3f8ed20f9877a7823f718112053a7d
Diksha Bajaj <diksha.bajaj@hpe.com>
2017-11-22 00:03:33 +0530

    Updated DBConnection

--- TestForSure/src/main/java/edu/tests/TestForSure/common/DBConnection.java
+++ TestForSure/src/main/java/edu/tests/TestForSure/common/DBConnection.java
@@ -3,7 +3,7 @@ package edu.tests.TestForSure.common;
 import java.sql.*;
 public class DBConnection {
 	private static final String DATABASE_DRIVER = "com.mysql.jdbc.Driver";
-	static String DATABASE_URL = "jdbc:mysql://"+System.getenv("DATABASE_HOST")+":"+System.getenv("DATABASE_PORT")+"/"+System.getenv("DATABASE_NAME");
+	static String DATABASE_URL = "jdbc:mysql://"+System.getenv("DATABASE_HOST")+":"+System.getenv("DATABASE_PORT")+"/"+System.getenv("DATABASE_NAME")+"?zeroDateTimeBehavior=convertToNull";
 	static String DATABASE_USERNAME = System.getenv("DATABASE_USERNAME");//"root";;
 	static String DATABASE_PASSWORD = System.getenv("DATABASE_PASSWORD");//"root";;
 	
@@ -12,9 +12,13 @@ public class DBConnection {
 		try {
 			try {
 				Class.forName(DATABASE_DRIVER);
+				//DATABASE_URL += System.getenv("DATABASE_HOST")+":"+System.getenv("DATABASE_PORT")+"/"+System.getenv("DATABASE_NAME");
+				//DATABASE_USERNAME = System.getenv("DATABASE_USERNAME");//"root";
+				//DATABASE_PASSWORD = System.getenv("DATABASE_PASSWORD");//"root";
 			} catch (ClassNotFoundException e) {
 				return null;
 			}
+			System.out.println(DATABASE_URL+"-----"+DATABASE_USERNAME+"-----"+DATABASE_PASSWORD);
 			Connection connection = DriverManager.getConnection(DATABASE_URL, DATABASE_USERNAME,
 					DATABASE_PASSWORD);
 			return connection;
